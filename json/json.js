98424be3e968a978e88a3bf44c54134ac44a6e09
vidurasilva <kvidurachathuranga@gmail.com>
2020-08-08 22:55:15 +0530

    Project file

---
+++ json/json.js
@@ -0,0 +1,5 @@
+var jsonText =
+  '{"employees":[' +
+  '{"firstName":"John","lastName":"Doe" },' +
+  '{"firstName":"Anna","lastName":"Smith" },' +
+  '{"firstName":"Peter","lastName":"Jones" }]}';
