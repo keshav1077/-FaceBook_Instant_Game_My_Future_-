7775d3106c430bb9759298d7cdf048315daa7a3c
vidurasilva <kvidurachathuranga@gmail.com>
2020-08-15 23:24:39 +0530

    java Script

--- script/game.js
+++ script/game.js
@@ -1,7 +1,28 @@
 console.log("loaded");
 
-var carModol = false;
-document.getElementById("car_button").addEventListener("click", function () {
-  document.getElementById("demo").innerHTML = "Hello World";
-});
-console.log(carModol);
+function getel(id) {
+  console.log(id);
+  return document.getElementById(id);
+}
+
+// var carModol = false;
+// var startButton = getel("car_button");
+document.getElementById("car_button").onclick = function () {
+  startGame();
+  console.log("Selected Car benZ");
+};
+// startButton.addEventListener("click", startGame);
+
+function startGame() {
+  console.log("Selected Car");
+}
+document.getElementById("demo").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+  console.log("Selected Car toyota");
+}
+// document.getElementById("car_button").addEventListener("click", function () {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// });
+// console.log(carModol);
