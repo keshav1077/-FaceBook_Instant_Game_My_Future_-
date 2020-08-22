690e0aefb76d1b36d7d1e435080f5d17232b366a
vidurasilva <kvidurachathuranga@gmail.com>
2020-08-22 22:12:27 +0530

    js update

--- script/game.js
+++ script/game.js
@@ -32,19 +32,20 @@ startButton.addEventListener("click", function () {
   if (
     image.src.match("https://homepages.cae.wisc.edu/~ece533/images/cat.png")
   ) {
-    changeImage();
+    var imageSources = [
+      "https://homepages.cae.wisc.edu/~ece533/images/pool.png",
+      "https://homepages.cae.wisc.edu/~ece533/images/girl.png",
+      "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
+      "https://homepages.cae.wisc.edu/~ece533/images/cat.png",
+    ];
+    var time = 1000;
+    rotateImage(imageSources, time);
   } else {
-    rotateImage();
+    changeImage();
   }
 });
-function rotateImage() {
+function rotateImage(imageSources, time) {
   console.log("Hi");
-  var imageSources = [
-    "https://homepages.cae.wisc.edu/~ece533/images/pool.png",
-    "https://homepages.cae.wisc.edu/~ece533/images/girl.png",
-    "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
-    "https://homepages.cae.wisc.edu/~ece533/images/cat.png",
-  ];
 
   var index = 0;
   setInterval(function () {
@@ -54,19 +55,21 @@ function rotateImage() {
     console.log(index);
     document.getElementById("withtoung").src = imageSources[index];
     index++;
-  }, 1000);
+  }, time);
 }
 
 function changeImage() {
-  var image = document.getElementById("withtoung");
-
-  if (
-    image.src.match("https://homepages.cae.wisc.edu/~ece533/images/cat.png")
-  ) {
-    var girl = false;
-    image.src = "https://homepages.cae.wisc.edu/~ece533/images/girl.png";
-  } else {
-    girl = true;
-    image.src = "https://homepages.cae.wisc.edu/~ece533/images/cat.png";
-  }
+  var imageSources = ["https://homepages.cae.wisc.edu/~ece533/images/cat.png"];
+  var time = 2000000000;
+  rotateImage(imageSources, time);
+  // var image = document.getElementById("withtoung");
+  // if (
+  //   image.src.match("https://homepages.cae.wisc.edu/~ece533/images/cat.png")
+  // ) {
+  //   var girl = false;
+  //   image.src = "https://homepages.cae.wisc.edu/~ece533/images/girl.png";
+  // } else {
+  //   girl = true;
+  //   image.src = "https://homepages.cae.wisc.edu/~ece533/images/cat.png";
+  // }
 }
