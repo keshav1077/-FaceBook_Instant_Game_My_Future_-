c7f9e69f0e506698c47cabd6fdcda5679bec2753
vidurasilva <kvidurachathuranga@gmail.com>
2020-08-25 22:59:58 +0530

    JS update

--- script/game.js
+++ script/game.js
@@ -32,13 +32,19 @@ startButton.addEventListener("click", function () {
   var image = document.getElementById("withtoung");
   startButton.disabled = true;
   profileImage.disabled = false;
-  var imageSources = [
-    "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
-    "https://homepages.cae.wisc.edu/~ece533/images/boat.png",
-    "https://homepages.cae.wisc.edu/~ece533/images/girl.png",
-    "https://homepages.cae.wisc.edu/~ece533/images/cat.png",
-  ];
-  rotateImage(imageSources);
+  if (startButton.disabled) {
+    var imageSources = [
+      "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
+      "https://homepages.cae.wisc.edu/~ece533/images/boat.png",
+      "https://homepages.cae.wisc.edu/~ece533/images/girl.png",
+      "https://homepages.cae.wisc.edu/~ece533/images/cat.png",
+    ];
+    rotateImage(imageSources);
+  } else {
+    console.log("star button off" + startButton.disabled);
+    profileImage.src =
+      "https://homepages.cae.wisc.edu/~ece533/images/airplane.png";
+  }
 });
 
 function rotateImage(imageSources) {
@@ -57,8 +63,9 @@ function rotateImage(imageSources) {
 function changeImage() {
   console.log("Hi Change Image");
   startButton.disabled = false;
-  var imageSources = ["https://homepages.cae.wisc.edu/~ece533/images/cat.png"];
-  rotateImage(imageSources);
+  profileImage.src =
+    "https://homepages.cae.wisc.edu/~ece533/images/airplane.png";
+  //rotateImage(imageSources);
   // var image = document.getElementById("withtoung");
   // if (
   //   image.src.match("https://homepages.cae.wisc.edu/~ece533/images/cat.png")
