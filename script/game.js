a7a34648614cef65b1e206ee4c528b5aeb01ae11
vidurasilva <kvidurachathuranga@gmail.com>
2020-09-02 18:53:19 +0530

    Update style and index file

--- script/game.js
+++ script/game.js
@@ -26,52 +26,88 @@ function getel(id) {
 var startButton = getel("startGame");
 var profileImage = getel("withtoung");
 var gamePlay = getel("carbutton");
+// Get the modal
+var modal = getel("myModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//Global veriable
+var workOrNot;
+var indexOfImage;
+var index = 0;
+var winnigImage = "./image/dog_0.jpg";
+var greenColor = "#3e8e41";
+var redColor = "rgb(233 103 120)";
+
+var imageSources = [
+  winnigImage,
+  "./image/dog_1.jpg",
+  "./image/dog_2.jpg",
+  "./image/dog_3.jpg",
+  "./image/dog_4.jpg",
+  "./image/dog_5.jpg",
+  "./image/dog_6.jpg",
+  "./image/dog_7.jpg",
+];
 
 startButton.addEventListener("click", function () {
   console.log("Hi Push Start");
-  var image = document.getElementById("withtoung");
   startButton.disabled = true;
+  startButton.style.backgroundColor = redColor;
+  startButton.value = "Click on image and Win";
+  //changeVisibilityOfButton();
   profileImage.disabled = false;
-  if (startButton.disabled) {
-    var imageSources = [
-      "./image/IMG_1058.jpg",
-      "./image/facetest bill.jpg",
-      "./image/IMG_1062.jpg",
-      "./image/StayAtHome.png",
-    ];
-    rotateImage(imageSources);
-  } else {
-    console.log("star button off" + startButton.disabled);
-    profileImage.src = "./image/IMG_1058.jpg";
-  }
+  rotateImage();
 });
 
-function rotateImage(imageSources) {
+function rotateImage() {
   console.log("Hi Image Roteter");
-  var index = 0;
-  setInterval(function () {
+  workOrNot = setInterval(function () {
     if (index === imageSources.length) {
       index = 0;
     }
-    console.log(index);
-    document.getElementById("withtoung").src = imageSources[index];
+    console.log("index " + index);
+    // console.log("randome number");
+
+    indexOfImage = Math.round(Math.random() * Math.floor(7));
+    console.log("randome number " + indexOfImage);
+    document.getElementById("withtoung").src = imageSources[indexOfImage];
     index++;
-  }, 1000);
+  }, 100);
 }
 
 function changeImage() {
   console.log("Hi Change Image");
+  runOrStopButton = false;
+  clearInterval(workOrNot);
   startButton.disabled = false;
-  profileImage.src = "/image/IMG_1058.jpg";
-  //rotateImage(imageSources);
-  // var image = document.getElementById("withtoung");
-  // if (
-  //   image.src.match("https://homepages.cae.wisc.edu/~ece533/images/cat.png")
-  // ) {
-  //   var girl = false;
-  //   image.src = "https://homepages.cae.wisc.edu/~ece533/images/girl.png";
-  // } else {
-  //   girl = true;
-  //   image.src = "https://homepages.cae.wisc.edu/~ece533/images/cat.png";
-  // }
+  startButton.style.backgroundColor = greenColor;
+  startButton.value = "Push to Start & Try It Again";
+  //changeVisibilityOfButton();
+  profileImage.src = imageSources[indexOfImage];
+  profileImage.disabled = true; //index must less than 8.
+
+  if (imageSources[indexOfImage] == winnigImage) {
+    modal.style.display = "block";
+  }
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+function changeVisibilityOfButton() {
+  if (startButton.style.display === "none") {
+    startButton.style.display = "block";
+    startButton.style.color = greenColor;
+  } else {
+    startButton.style.display = "none";
+    startButton.style.color = redColor;
+  }
 }
